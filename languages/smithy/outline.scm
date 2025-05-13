793600bcc6049f8faa99f8f797ab620f9530f816
Jakub Kozłowski <kubukoz@gmail.com>
2025-05-13 13:43:17 +0200

    Add missing outline captures

--- languages/smithy/outline.scm
+++ languages/smithy/outline.scm
@@ -1,6 +1,10 @@
+(simple_shape_statement (primitive) @context (identifier) @name) @item
+(enum_statement  ["enum" "intEnum"] @context (identifier) @name) @item
+(list_statement "list" @context (identifier) @name) @item
+(map_statement "map" @context (identifier) @name) @item
+(set_statement "set" @context (identifier) @name) @item
 (structure_statement "structure" @context (identifier) @name) @item
 (union_statement "union" @context (identifier) @name) @item
-(list_statement "list" @context (identifier) @name) @item
-(enum_statement "enum" @context (identifier) @name) @item
-(operation_statement "operation" @context (identifier) @name) @item
 (service_statement "service" @context (identifier) @name) @item
+(operation_statement "operation" @context (identifier) @name) @item
+(resource_statement "resource" @context (identifier) @name) @item
